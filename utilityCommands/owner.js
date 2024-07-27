90222b40d285724b710bbfebd1a0325c75f000bb
ponarts1 <153155210+ponarts1@users.noreply.github.com>
2024-07-27 19:20:38 +0800

    Update owner.js

--- utilityCommands/owner.js
+++ utilityCommands/owner.js
@@ -1,17 +1,16 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    name: 'owner',
-    description: 'Bot owner info',
+    name: "update",
+    description: "Update Bot",
     execute(message, args) {
-        const youtubeLink = 'https://www.youtube.com/@GlaceYT';
-        const InstagramLink = 'https://www.instagram.com/glaceytt';
         const embed = new EmbedBuilder()
-            .setColor('#FFFFFF')
-            .setTitle(' 🫅 Owner Info')
-            .setDescription(`__**About me**__:\n 🤖 Myself Shiva aka GlaceYT. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [GlceYT](${youtubeLink})\n 💙 [GlaceYTT](${InstagramLink})`)
+            .setColor("#FFFFFF")
+            .setTitle("📜 Bot Update Logs")
+            .setDescription(
+                `__**Updates**__:\n 📆 Sabtu,27 Juli 2024 : Bot telah kembali dan diperbarui!`,
+            )
             .setTimestamp();
-
 
         message.reply({ embeds: [embed] });
     },
