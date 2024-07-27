981ebe25e717af3cd14e23130d037394aac2fe18
ponarts1 <153155210+ponarts1@users.noreply.github.com>
2024-07-27 19:18:19 +0800

    Update support.js

--- basicCommands/support.js
+++ basicCommands/support.js
@@ -1,28 +1,29 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require("discord.js");
 const db = require("../mongodb");
 module.exports = {
-    name: 'support',
-    description: 'support server of this Bot',
+    name: "support",
+    description: "support server of this Bot",
     execute(message, args) {
-        const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+        const supportServerLink = "https://discord.gg/8Cc8df9Fdg";
         const embed = new EmbedBuilder()
-            .setColor('#b300ff')
+            .setColor("#b300ff")
             .setAuthor({
-              name: 'Support Server',
-              iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://discord.gg/xQF9f9yUEM'
-          })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+                name: "Support Server",
+                iconURL:
+                    "https://media.discordapp.net/attachments/1266656449896120371/1266656571023167498/98603-verified.gif?ex=66a5f15b&is=66a49fdb&hm=66309b618678e889136db85b8c78e0c2dad3946efc43440cfff3dffae8976947&=&width=43&height=43",
+                url: "https://discord.gg/8Cc8df9Fdg",
+            })
+            .setDescription(
+                `➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}`,
+            )
+            .setImage(
+                "https://media.discordapp.net/attachments/862865027731685399/1266655479547826247/standard_1.gif?ex=66a5f057&is=66a49ed7&hm=00c8612a2d953e0ea8d5d3e29e8e5a8388489da868f7339393d78438403341fd&=&width=539&height=215",
+            )
             .setTimestamp();
 
         message.reply({ embeds: [embed] });
     },
 };
-
 
 /*
 
